1d013973c7398572f08cf82403e64f2e39d7665e
Nguyen Dat <vandat9xiloveyou@gmail.com>
2022-02-08 16:47:56 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-range-slider-ts'
+import 'react-range-slider-ts/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
